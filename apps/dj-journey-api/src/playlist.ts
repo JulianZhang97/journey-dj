7efd0b2f90ed76f9559a2efe438f56ae050c8007
Julian <zhangzhuoen@gmail.com>
2022-06-05 14:06:30 -0400

    Playlist top songs and weighted shuffle algo complete

--- apps/dj-journey-api/src/playlist.ts
+++ apps/dj-journey-api/src/playlist.ts
@@ -1,39 +1,44 @@
-import { AxiosRequestConfig } from 'axios';
 import * as express from 'express';
-const router = express.Router();
-
 import { getTripDuration } from './services/mapbox';
-import { generatePlaylist, generateUserTopItems } from './services/spotify';
-
-
+import {
+  generatePlaylist
+} from './services/spotify';
+const router = express.Router();
 
 router.get('/', async (req, res) => {
   //TODO: Need request typings here
 
-  const {origin, destination, travelType, spotifyToken, tripName} = req.query;
+  const { origin, destination, travelType, spotifyToken, tripName } = req.query;
 
-    //Request needs to include origin/destination + Spotify user credentails 
-    const tripDuration = await getTripDuration(origin as string, destination as string, travelType as string);
+  //Request needs to include origin/destination + Spotify user credentails
+  const tripDurationSeconds = await getTripDuration(
+    origin as string,
+    destination as string,
+    travelType as string
+  );
 
-    const generatedPlaylist = await generatePlaylist(tripDuration, spotifyToken as string);
+  const generatedPlaylist = await generatePlaylist(
+    tripDurationSeconds,
+    spotifyToken as string,
+    tripName as string
+  );
 
-    res.send({ 
-      tripDurationSeconds: tripDuration,
-      playlist: generatedPlaylist});
+  res.send({
+    tripDurationSeconds,
+    playlist: generatedPlaylist,
   });
+});
 
+// router.get('/topSongs', async (req, res) => {
+//   //TODO: Need request typings here
 
-  router.get('/topSongs', async (req, res) => {
-    //TODO: Need request typings here
-  
-    const {spotifyToken} = req.query;
-    
-      const topSongs = await generateUserTopItems(spotifyToken as string);
+//   const { spotifyToken } = req.query;
 
-      res.send({ 
-        topSongs});
-    });
+//   const topSongs = await generateUserTopItems(spotifyToken as string);
+//   res.send({
+//     topSongs,
+//   });
+// });
 
+export { router as playlist };
 
-
-export { router as playlist }
--- apps/dj-journey-api/src/services/spotify.ts
+++ apps/dj-journey-api/src/services/spotify.ts
@@ -4,6 +4,8 @@ import { topLimit } from '../constants';
 axios.defaults.baseURL = 'https://api.spotify.com/v1';
 axios.defaults.headers['Content-Type'] = 'application/json';
 
+
+// TODO: Move these into typings file
 interface TopTrack {
     uri: string,
     popularityScore: number,
@@ -36,18 +38,59 @@ async function getUserTopArtists(timeRange: string){
 }
 
 
-export async function generatePlaylist(tripDurationSeconds: number, spotifyToken: string){
-    axios.defaults.headers['Authorization'] = `Bearer ${spotifyToken}`;
+function weightedSongSort(songA: TopTrack, songB: TopTrack): number {
+    //Based on "Weighted Random Sampling algorithm from https://softwareengineering.stackexchange.com/questions/233541/how-to-implement-a-weighted-shuffle"
+    const aVal = Math.random() ** (1.0 / songA.popularityScore);
+    const bVal = Math.random() ** (1.0 / songB.popularityScore);
 
-    // // try{
-    // // }
-    // catch(error){
-    //     console.error("An error occured!", error);
-    // } 
+    return bVal - aVal;
 }
 
 
-export async function generateUserTopItems(spotifyToken: string){
+export async function generatePlaylist(tripDurationSeconds: number, spotifyToken: string, tripName: string){
+    axios.defaults.headers['Authorization'] = `Bearer ${spotifyToken}`;
+
+    try {
+        // Get all of user's top songs weighted shuffled based on their popularityScore
+        const topUserSongs = await generateUserTopItems(spotifyToken);
+
+        const playlistToCreate = [];
+        let playlistTotalDurationSeconds = 0;
+        let curIndex = 0;
+        
+        // Get total duration of trip, start adding songs to playlist. While duration of playlist is < total duration, keep adding songs
+        while(playlistTotalDurationSeconds < tripDurationSeconds && curIndex < topUserSongs.length - 1){
+            const songToAdd = topUserSongs[curIndex];
+            playlistToCreate.push(songToAdd);
+            playlistTotalDurationSeconds += songToAdd.durationMs / 1000
+            curIndex += 1;
+        }
+
+        // If the playlist duration is shorter than trip duration, pass warning to client that playlist length is insufficient due to long trip duration
+        if (playlistTotalDurationSeconds < tripDurationSeconds) {
+            const warningMsg = "Warning! Due to long trip duration, created playlist does not have enough songs for then entire trip.";
+        }        
+
+        // Create spotify playlist with these songs and return this playlist to the client
+
+        return playlistToCreate;
+
+    }
+    catch(error) {
+        console.error("An error occured while generating the playlist!", error);
+    }
+
+
+    // Future TODO: 
+    // If all songs from user's top items are added and playlist duration is still less than trip duration, load 5 songs from users' top 20 artists
+    // Go through 100 songs from top artists and randomly add them to the trip playlist until reaching the trip duration limit or until all 100 songs are added
+
+    // Add additional songs based on "song suggestions" if additional songs still needed
+}
+
+
+
+export async function generateUserTopItems(spotifyToken: string): Promise<TopTrack[]> {
     try{
         axios.defaults.headers['Authorization'] = `Bearer ${spotifyToken}`;
 
@@ -63,7 +106,7 @@ export async function generateUserTopItems(spotifyToken: string){
 
         //Promise await all and combine them together in an array 
         allTracks.forEach((trackRes) => {
-            topItemArray = topItemArray.concat(trackRes.data.map((topSongItem) => getTopItemInfo(topSongItem)));
+            topItemArray = topItemArray.concat(trackRes.data.items.map((topSongItem) => getTopItemInfo(topSongItem)));
         });
 
         //Iterate through array and add to top track map so each track is added only once
@@ -72,8 +115,8 @@ export async function generateUserTopItems(spotifyToken: string){
                 favSongMap[item.id] = {uri: item.uri, popularityScore: item.popularityScore, durationMs: item.durationMs};
         });
 
-        //Return map as array of top songs (should be unique)
-        return favSongMap;
+        //Return map as array of top songs (should be unique) WEIGHTED SORTED by user song popularity 
+        return Object.values(favSongMap).sort(weightedSongSort);
     }
 
     catch(error){
